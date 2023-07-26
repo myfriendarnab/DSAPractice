f8ce9ae56ea8d3a1ee0497a8398c60db8592bd23
arnab bhattacharya <myfriendarnab@gmail.com>
2023-07-26 16:46:28 +0530

    Dev (#1)
    * updating devcontainer
    
    * 1672 initial
    
    * roman to int
    
    * added code to debug locally
    
    * normal solution and setting for lambda solution
    
    * some more updates
    
    * pytest issue solved
    
    * updates
    
    * roman to int completed
    
    ---------
    
    Co-authored-by: arnabmyfriend <AT-Education@users.noreply.github.com>

---
+++ leetcode/top100voted/strings/romantoint/Solution.py
@@ -0,0 +1,40 @@
+class SolutionRecursion():
+ 
+    def roman_to_int(self, s: str) -> int:
+        if not s:
+            return 0
+        if len(s)<=1:
+            return self.__get_number(s[0])
+        sign = -1 if self.__get_number(s[0])<self.__get_number(s[1]) else 1
+        return self.__get_number(s[0])*sign + self.roman_to_int(s[1:])
+
+    def __get_number(self, first_char:chr) -> int:
+        switch={
+            '':0,
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        return switch.get(first_char,"Invalid input")
+
+class Solutionlambda:
+        def roman_to_int_lambda(self, s: str) -> int:
+            parse={
+                'I':1,
+                'V':5,
+                'X':10,
+                'L':50,
+                'C':100,
+                'D':500,
+                'M':1000
+            }
+            
+            s=s.replace("IV", "IIII").replace("IX", "IIIIIIIII")
+            s=s.replace("XL", "XXXX").replace("XC", "XXXXXXXXX")
+            s=s.replace("CD", "CCCC").replace("CM", "CCCCCCCCC")
+
+            return sum(map(lambda x: parse[x],s))
