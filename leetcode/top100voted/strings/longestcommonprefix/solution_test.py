c0f3c7fe64a2400cb27f9fe4c7006a3e2706991a
arnab bhattacharya <myfriendarnab@gmail.com>
2023-07-26 15:53:48 +0000

    checking issue

--- leetcode/top100voted/strings/longestcommonprefix/solution_test.py
+++ leetcode/top100voted/strings/longestcommonprefix/solution_test.py
@@ -1,5 +1,5 @@
 import solution2
 
 def test_Solution_given_nothing_should_return_nothing():
-    assert solution2.Solution2().longestCommonPrefix('')==''
+    assert solution2.Solution2().longestCommonPrefix('')== ''
     
