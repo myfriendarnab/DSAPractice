1e9bf465734c3604e081133eaaa0e6ec93c1e6a4
arnab bhattacharya <myfriendarnab@gmail.com>
2023-07-28 09:12:52 +0000

    fixed test cases issues

--- leetcode/top100voted/strings/longestcommonprefix/solution2.py
+++ leetcode/top100voted/strings/longestcommonprefix/solution2.py
@@ -5,14 +5,31 @@ from collections import deque
 class Solution2:
     '''class for leetcode problem of finding longest'''
     def longest_common_prefix(self, strs: List[str]) -> str:
-        '''main entry method'''
+        '''this method comapares first string with others in following way:
+            input:['reflow','flow','flower','flour','reflex']; first string:'reflow'
+            we are incrementing an offset till iterating through whole 'reflow' and taking those substrings -> 'reflow'[:2]='re'
+            if substr_in_list and previous char is also pesent in list of LinkedList
+                appending the value to linked list
+            else
+                creating a new linked list entry to the list
+            if NOT substr_in_list
+                trimming the offset characters from first string and checking the rest --> 'reflow'[2:]='flow'
+            largest_common_prefixes[0]=''
+            largest_common_prefixes[1]='f'
+            largest_common_prefixes[1]='f'>>'l'
+
+            longest linked list is the result
+        '''
+        # initializations
         substr_in_list=(lambda ss,l:
                      True if 
                         len(list(filter(lambda x:x.find(ss)!=-1,l)))==len(l)
                         else False)
         compare=strs[0]
         offset=1
-        largest_common_prefixes=[]
+        largest_common_prefixes=[''] # initializing for no match as first item
+
+        # calculation
         while offset<len(compare):
             if substr_in_list(compare[:offset],strs[1:]):
                 if any(largest_common_prefixes) and compare[:offset-1]==largest_common_prefixes[-1]:
@@ -28,5 +45,5 @@ class Solution2:
         largest_common_prefixes_strings = list(map(lambda x:''.join(x),largest_common_prefixes))
         return max(largest_common_prefixes_strings)
 
-res=Solution2().longest_common_prefix(['reflow','flow','flower','flour','reflex'])
-print(res)
+# res=Solution2().longest_common_prefix(["flower","flow","flight"])
+# print(res)
--- leetcode/top100voted/strings/longestcommonprefix/test_solution2.py
+++ leetcode/top100voted/strings/longestcommonprefix/test_solution2.py
@@ -2,3 +2,5 @@ import solution2
 
 def test_Solution_given_nothing_should_return_nothing():
     assert solution2.Solution2().longest_common_prefix(['reflow','flow','flower','flour','reflex'])=='fl'
+    assert solution2.Solution2().longest_common_prefix(["flower","flow","flight"])=='fl'
+    assert solution2.Solution2().longest_common_prefix(["dog","racecar","car"])==''
